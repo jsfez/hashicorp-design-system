ea5dc66a83e6738b365e6bea167041794b651ceb
Zack Moore <zack.moore@hashicorp.com>
2026-07-06 10:02:57 -0400

    wip

--- packages/mcp/src/resources/index.ts
+++ packages/mcp/src/resources/index.ts
@@ -4,12 +4,12 @@
  */
 
 import { withSafeResourceHandler } from "./utils.js";
-import FLIGHT_ICONS_RESOURCES from "./flight-icons/index.js";
+import TOKENS_RESOURCES from "./tokens/index.js";
 
 import type { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import type { McpResource } from "./types.js";
 
-const RESOURCES: McpResource[] = [FLIGHT_ICONS_RESOURCES];
+const RESOURCES: McpResource[] = [...TOKENS_RESOURCES];
 
 export function registerResources(server: McpServer) {
   for (const resource of RESOURCES) {
